6fae06b33a68bad3121dae9edebf6b9297ce3c53
Rajiv Bakulesh Shah <brainix@gmail.com>
2017-12-26 04:44:22 -0800

    Run doctests as part of unit test suite

---
+++ tests/test_doctests.py
@@ -0,0 +1,30 @@
+#-----------------------------------------------------------------------------#
+#   test_doctests.py                                                          #
+#                                                                             #
+#   Copyright (c) 2017, Rajiv Bakulesh Shah, original author.                 #
+#   All rights reserved.                                                      #
+#-----------------------------------------------------------------------------#
+
+
+
+import doctest
+import importlib
+import os
+import unittest
+
+
+
+class DoctestTests(unittest.TestCase):
+    def _modules(self):
+        test_dir = os.path.dirname(__file__)
+        source_dir = os.path.dirname(test_dir)
+        source_files = (f for f in os.listdir(source_dir) if f.endswith('.py'))
+        for source_file in source_files:
+            module_name = os.path.splitext(source_file)[0]
+            module = importlib.import_module(module_name)
+            yield module
+
+    def test_doctests(self):
+        for module in self._modules():
+            results = doctest.testmod(m=module)
+            assert not results.failed
