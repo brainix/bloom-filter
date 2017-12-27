74f920f61b5bc3e3410692d89cdd383bf9c1c30b
Rajiv Bakulesh Shah <brainix@gmail.com>
2017-12-26 19:53:56 -0800

    Refactor bloom from a module to a package
    This will keep things maintainable if we want to add more modules.

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -17,11 +17,12 @@ import unittest
 class DoctestTests(unittest.TestCase):
     def _modules(self):
         test_dir = os.path.dirname(__file__)
-        source_dir = os.path.dirname(test_dir)
+        package_dir = os.path.dirname(test_dir)
+        source_dir = os.path.join(package_dir, 'bloom')
         source_files = (f for f in os.listdir(source_dir) if f.endswith('.py'))
         for source_file in source_files:
             module_name = os.path.splitext(source_file)[0]
-            module = importlib.import_module(module_name)
+            module = importlib.import_module('bloom.{}'.format(module_name))
             yield module
 
     def test_doctests(self):
