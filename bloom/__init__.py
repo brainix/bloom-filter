394d3ad8c5ecc170b9adfcf3e02fc2c7b0277697
Rajiv Bakulesh Shah <brainix@gmail.com>
2017-12-26 19:57:54 -0800

    Refactor bloom from a module to a package (#5)
    This will keep things maintainable if we want to add more modules.

---
+++ bloom/__init__.py
@@ -0,0 +1,10 @@
+#-----------------------------------------------------------------------------#
+#   __init__.py                                                               #
+#                                                                             #
+#   Copyright (c) 2017, Rajiv Bakulesh Shah, original author.                 #
+#   All rights reserved.                                                      #
+#-----------------------------------------------------------------------------#
+
+
+
+from .bloom import BloomFilter
